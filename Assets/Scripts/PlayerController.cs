93c8c52bd76115bc61426cd57f5b9a2d646567c4
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2020-10-22 00:38:48 -0300

    Vehicle now rotates accordingly to players movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float fowardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);        
+        // Get player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        fowardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fowardInput);
+        // Turn the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
